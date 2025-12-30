2dfa3ce14363959e29873b1388e1c63b3615bc32
yogendraoplify <yogendra.singh@oplify.in>
2025-12-30 17:53:19 +0530

    feat: enhance candidate management with favorite functionality and pagination

--- store/slices/candidateSlice.js
+++ store/slices/candidateSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   candidates: [],
+  shortlistedCandidates: [],
+  totalPages: 0,
 };
 
 export const candidateReducer = createSlice({
@@ -8,11 +10,47 @@ export const candidateReducer = createSlice({
   initialState,
   reducers: {
     setCandidates: (state, action) => {
-      state.candidates = action.payload;
+      state.candidates = action.payload.students;
+      state.totalPages = action.payload.pagination.totalPages;
+    },
+    setShortlistedCandidates: (state, action) => {
+      state.shortlistedCandidates = action.payload.students;
+      state.totalPages = action.payload?.pagination?.totalPages || 1;
+    },
+    addToFavorite: (state, action) => {
+      const { companyId, candidateId } = action.payload;
+      state.candidates = state.candidates.map((candidate) =>
+        candidate.id === candidateId
+          ? {
+              ...candidate,
+              favoritedBy: [
+                ...candidate.favoritedBy,
+                { company: { id: companyId } },
+              ],
+            }
+          : candidate
+      );
+    },
+    removeFromFavorite: (state, action) => {
+      const { companyId, candidateId } = action.payload;
+      state.candidates = state.candidates.map((candidate) =>
+        candidate.id === candidateId
+          ? {
+              ...candidate,
+              favoritedBy: candidate.favoritedBy.filter(
+                (fav) => fav.company.id !== companyId
+              ),
+            }
+          : candidate
+      );
     },
   },
 });
 
-export const { setCandidates } = candidateReducer.actions;
-
+export const {
+  setCandidates,
+  setShortlistedCandidates,
+  addToFavorite,
+  removeFromFavorite,
+} = candidateReducer.actions;
 export default candidateReducer.reducer;
