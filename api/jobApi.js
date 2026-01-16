0aa88ff1743457c2e3f56b8420dacea4b4e4a38a
yogendraoplify <yogendra.singh@oplify.in>
2026-01-16 13:09:42 +0530

    feat: Implement job application status update functionality and enhance job applicants handling

--- api/jobApi.js
+++ api/jobApi.js
@@ -9,4 +9,10 @@ export const appliedJobApi = () => axios.get(`/student/job-applications`);
 export const companyjobApi = (params) =>
   axios.get(`/company/created-jobs`, { params });
 
-export const jobApplicantsApi = (params) => axios.get(`/company/created-job/job-applications`, { params }); 
+export const jobApplicantsApi = (params) =>
+  axios.get(`/company/created-job/job-applications`, { params });
+
+export const updateApplicationStatusApi = (applicationId, newStatus) =>
+  axios.patch(`/company/update-job-application-status?id=${applicationId}`, {
+    status: newStatus,
+  });
